094d1792d53798f3adfbc6c1df9a5bd55bbff5d3
sami157 <sami.eee157@gmail.com>
2025-11-09 02:25:33 +0600

    installed necessary packages for client

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,28 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter } from "react-router";
+import { RouterProvider } from "react-router/dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Hello World</div>,
+  },
+]);
+
+const root = document.getElementById("root");
+
+ReactDOM.createRoot(root).render(
+  <RouterProvider router={router} />,
+);
+
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+  <RouterProvider router={router}>
+    <StrictMode>
     <App />
-  </StrictMode>,
+  </StrictMode>
+  </RouterProvider>
 )
