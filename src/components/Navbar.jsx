12496008efe4135fbd25a6bf9feb76dc2f027a1b
sami157 <sami.eee157@gmail.com>
2025-11-10 13:16:26 +0600

    added tailwind and daisyui

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,8 +2,9 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div>
-      Navbar
+    <div className='px-4 py-2 flex justify-between bg-gray-300'>
+      <div>Logo</div>
+      <div>Navlinks</div>      
     </div>
   )
 }
