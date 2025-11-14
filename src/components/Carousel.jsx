a6cd39f172a32365caa26457911320d7db75e6e4
sami157 <sami.eee157@gmail.com>
2025-11-14 20:35:39 +0600

    hero section carousel done

---
+++ src/components/Carousel.jsx
@@ -0,0 +1,37 @@
+const Carousel = () => {
+    return (
+        <div className="md:w-3/5 mx-auto my-4">
+            <div className="carousel rounded-lg">
+                <div id="slide1" className="carousel-item relative w-full flex flex-col md:justify-center bg-white/50 glass h-60 md:h-100">
+                    <p className="md:p-10 p-4 text-center md:text-2xl md:mb-0 mb-10">StudyMate is a MERN Stack web platform designed to help students connect and collaborate for better learning outcomes.It enables users to find study partners based on subjects, learning preferences, or nearby locations, making education more interactive, engaging, and goal-oriented.</p>
+                    <div className="absolute left-5 right-5 top-4/5 flex -translate-y-1/2 transform justify-between">
+                        <a href="#slide3" className="btn btn-circle">❮</a>
+                        <a href="#slide2" className="btn btn-circle">❯</a>
+                    </div>
+                </div>
+                <div id="slide2" className="carousel-item relative w-full flex flex-col md:justify-center bg-white/50 glass">
+                    <p className="md:p-10 p-4 text-center md:text-2xl md:mb-0 mb-10">
+                        Built with the MERN Stack (MongoDB, Express, React, Node.js), this project focuses on practical full-stack development through CRUD operations, Firebase authentication, and a responsive, user-friendly interface that enhances real-world learning collaboration
+                    </p>
+                    <div className="absolute left-5 right-5 top-4/5 flex -translate-y-1/2 transform justify-between">
+                        <a href="#slide1" className="btn btn-circle">❮</a>
+                        <a href="#slide3" className="btn btn-circle">❯</a>
+                    </div>
+                </div>
+                <div id="slide3" className="carousel-item relative w-full flex flex-col md:justify-center bg-white/50 glass">
+                    <p className="md:p-10 p-4 text-center md:text-2xl md:mb-0 mb-10">
+                        Schedule regular sessions with your StudyMate so you both keep each other
+                        accountable — no more “I’ll start tomorrow”.
+                    </p>
+                    <div className="absolute left-5 right-5 top-4/5 flex -translate-y-1/2 transform justify-between">
+                        <a href="#slide2" className="btn btn-circle">❮</a>
+                        <a href="#slide1" className="btn btn-circle">❯</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    )
+}
+
+export default Carousel
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,22 +1,26 @@
 import React from 'react';
 import { useLoaderData } from 'react-router'
 import { NavLink } from 'react-router'
+import Carousel from '../components/Carousel';
 
 const Home = () => {
     const topProfiles = useLoaderData().data
     return (
-        <div className='flex flex-col gap-4'>
-            <div className='flex gap-10'>
-                {
-                    topProfiles.map(
-                        (profile) => (
-                            <div key={profile._id}>
-                                <p>{profile.name}</p>
-                                <NavLink to={`/partner-profile/${profile._id}`}><p className='hover:bg-gray-100'>View Profile</p></NavLink>
-                            </div>
+        <div className='px-4'>
+            <Carousel></Carousel>
+            <div className='flex flex-col gap-4'>
+                <div className='flex gap-10'>
+                    {
+                        topProfiles.map(
+                            (profile) => (
+                                <div key={profile._id}>
+                                    <p>{profile.name}</p>
+                                    <NavLink to={`/partner-profile/${profile._id}`}><p className='hover:bg-gray-100'>View Profile</p></NavLink>
+                                </div>
+                            )
                         )
-                    )
-                }
+                    }
+                </div>
             </div>
         </div>
     );
